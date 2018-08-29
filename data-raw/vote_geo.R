97068f35e158bfb513a27acc9ceb330974a86bcc
Matthew Routley <matt@routleynet.org>
2018-08-29 16:58:51 -0400

    Process shapefiles and simplify to wards and areas

--- data-raw/vote_geo.R
+++ data-raw/vote_geo.R
@@ -14,7 +14,22 @@ for (year in years) {
     unzip(paste(shapefile_prefix, year, ".zip", sep=""), exdir=paste0("data-raw/", year))
   }
 
-  assign(paste("geo", year, sep = "-"), sf::st_read(paste0("data-raw/", year)))
+  assign(paste("geo", year, sep = "_"),
+         sf::st_read(paste0("data-raw/", year)) %>%
+           sf::st_transform(crs = "+init=epsg:4326") %>% # Not strictly necessary, since already using this projection
+           dplyr::mutate(ward = as.integer(stringr::str_sub(AREA_LONG, 1, 2)), # Extract Ward ID
+                         area = as.integer(stringr::str_sub(AREA_LONG, -3, -1)), # Extract Area ID
+                         year = year) %>%
+           dplyr::select(ward, area, year, geometry)
+         )
 }
+geo_2003 <- geo_2006 %>%
+  mutate(year = 2003)
 rm(year, years, base_shapefile_url, shapefile_prefix)
+
+geo <- dplyr::bind_rows(geo_2006, geo_2010, geo_2014)
+
+#TODO: Merge years
+#TODO: Join with vote data and map to confirm
+#TODO: Decide what to do about area 99s
 
